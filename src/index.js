65de31897e99b0a92c235ffe1aaed0ae38072415
unknown <angeldeborah997@gmail.com>
2023-02-26 14:32:18 +0100

    update our menu-items to navigate withRouter HOC

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-
-
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
-reportWebVitals();
